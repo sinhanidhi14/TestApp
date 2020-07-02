a1d201c32053312b08bde6d51355698ccfacb327
Nidhi Sinha <v-sinidh@microsoft.com>
2020-07-02 12:26:14 -0700

    Add project files.

---
+++ CernerApp/Controllers/CernerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CernerApp.Controllers
+{
+    public class CernerController : Controller
+    {
+        public IActionResult Launch()
+        {
+            return View();
+        }
+
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+        public IActionResult Health()
+        {
+            return View();
+        }
+    }
+}
